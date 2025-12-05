793cde38cfbb490d707a4025f315b351d3e7ab20
vercel-24-7 <rasathemi@gmail.com>
2025-12-05 20:08:52 +0530

    Update SESSION_ID with a new default value

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=YVRBTDyC#khXceg5Zk-ZuR-I0pkqfkS2Jr9W4ecILjYN94yc6ub0' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };

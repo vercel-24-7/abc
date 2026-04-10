f4157faa647aa3b2ab972a1e2b62f0cf4b8c5199
vercel-24-7 <rasathemi@gmail.com>
2026-04-10 07:32:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=QZIDELrI#o3IvrDbwwM4jAO9zABHbtbuJ-RzmENoJlN3fyIuc-gI' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=hQYUCSSY#30FUBJ-QF9Fz7vVRRTgsSz2DselK0eNfgsuJ22q1HjA' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
